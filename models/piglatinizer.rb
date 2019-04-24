88051600faa9e2fa5b2b6957197f23f4edc75d40
github username <github email address>
2019-04-24 07:31:57 -0500

    done

---
+++ models/piglatinizer.rb
@@ -0,0 +1,39 @@
+class PigLatinizer
+
+    attr_accessor :words, :result
+    @@all = []
+    def initialize
+        @@all << self
+    end
+
+    def piglatinize(string)
+        a = string.split(" ")
+        b = a.map {|word| piglatinize_word(word)}
+        b.join(" ")
+    end
+
+    def piglatinize_word(word)
+        word2 = word.downcase
+        if first_vowel_index(word2) == 0
+            word = word+"way"
+        else 
+            word = word[first_vowel_index(word2)..]+word[0..first_vowel_index(word2)-1]+"ay"
+        end
+        x = word
+    end
+
+    def first_vowel_index(word)
+        i = 0
+        vowels = ['a','e','i','o','u']
+        first_vowel_index = word.split("").each do |letter|
+            if vowels.include?(letter)
+                return i
+            end
+            i+=1
+        end 
+    end
+
+    def self.all
+        @@all 
+    end
+end
